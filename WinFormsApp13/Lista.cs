746935f8e2fe1bf73b73fa7b887ad13e031099ed
Davide Zedda <davide.zedda@giua.edu.it>
2024-02-11 19:17:51 +0100

    solved push bug after deserialize

--- WinFormsApp13/Lista.cs
+++ WinFormsApp13/Lista.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Newtonsoft.Json;
 
 namespace WinFormsApp13
@@ -11,31 +13,36 @@ namespace WinFormsApp13
     {
         Nodo inizio;
         Nodo fine;
+        
         public Lista(string Elemento)
         {
             Nodo x = new Nodo(Elemento);
             inizio = x;
-            fine = x;               
+            fine = x;
         }
 
         public void Push(string Elemento)
         {
-            Nodo x= new Nodo(Elemento);
+            Nodo x = new Nodo(Elemento);
+            fine = findLast(); //ci si assicura che il puntato da fine sia proprio l'ultimo nodo
             fine.Successivo = x;
-            fine= x;
+            fine = x;
         }
 
-        
+
 
         public Nodo Inizio
         {
-            get { return inizio; } 
+            get { return inizio; }
             set { inizio = value; }
         }
 
         public Nodo Fine
         {
-            get { return fine; }
+            get {
+                
+                return fine;
+            }
             set { fine = value; }
         }
 
@@ -47,6 +54,20 @@ namespace WinFormsApp13
         {
             Lista x;
             x = JsonConvert.DeserializeObject<Lista>(File.ReadAllText("lista.json"));
+            return x;
+        }
+        /// <summary>
+        /// trova il nodo che successivo = null
+        /// ovvero l'ultimo
+        /// </summary>
+        /// <returns></returns>
+        private Nodo findLast()
+        {
+            Nodo x = inizio;            
+            while (x.Successivo != null)
+            {
+                x = x.Successivo;                
+            }
             return x;
         }
 
